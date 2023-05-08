67c53635e9e8db0256a37e3aa2214f933df54471
jambtc <jambtc@gmail.com>
2023-05-08 17:58:47 +0200

    first update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # merkle-root-php
- Merkle Root Calculator
+Merkle Root Calculator per php
+
+
+## Usage
+
+```php
+require __DIR__ . '/src/MerkleRoot.php';
+
+
+// array di dati
+$data = ['abc', 'cde', 'efg', 'jki', 'lmn'];
+
+// inizializzo la classe
+$merkle = new MerkleRoot();
+
+// genero il Merkle root
+$root = $merkle->root($data);
+
+
+echo "Merkle root: $root\n";
+echo "Verifica Merkle root: " . ($merkle->verify($data, $root) ? 'SUCCESSO' : 'FALLITO'] . "\n";
+```
+
+Nell'esempio, il metodo `root` viene utilizzata per calcolare il Merkle Root a partire dalla lista di dati 'abc', 'cde', 'efg', 'jki', 'lmn', e viene stampato il risultato.
+
+Successivamente, il metodo `verify` viene utilizzata per verificare se i dati 'abc', 'cde', 'efg', 'jki', 'lmn' corrispondono al Merkle Root calcolato e viene stampato il risultato della verifica.
---
+++ src/MerkleRoot.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Merkle Root Calculator
+ * 
+ * Author: Sergio Casizzone
+ * Date: 03.05.2023
+ */
+
+class MerkleRoot
+{
+    /**
+     * Calcola il Merkle Root
+     * 
+     * La funzione calcola il Merkle Root a partire dai dati passati come argomento, utilizzando 
+     * la funzione hash SHA256 per calcolare gli hash dei singoli elementi e degli hash intermedi. 
+     * Viene restituito il Merkle Root, ovvero l'hash della radice del Merkle Tree.
+     * 
+     * @param array $data Array di hash
+     * @return string Merkle root
+     */
+    public function root(array $data): string
+    {
+        // Se il numero di elementi è dispari, duplica l'ultimo elemento
+        if (count($data) % 2 != 0) {
+            $data[] = end($data);
+        }
+
+        // Calcola gli hash dei singoli elementi
+        foreach ($data as $element) {
+            $merkle_tree[] = hash('sha256', $element);
+        }
+
+        // Calcola gli hash intermedi
+        while (count($merkle_tree) > 1) {
+            $level = [];
+            for ($i = 0; $i < count($merkle_tree); $i += 2) {
+                $left = $merkle_tree[$i];
+                $right = isset($merkle_tree[$i + 1]) ? $merkle_tree[$i + 1] : $left;
+                $level[] = hash('sha256', $left . $right);
+            }
+            $merkle_tree = $level;
+        }
+
+        return $merkle_tree[0];
+    }
+
+    /**
+     * Verifica il Merkle Root
+     * 
+     * La funzione verifica se i dati passati come argomento corrispondono al Merkle Root passato 
+     * come secondo argomento. La funzione utilizza la funzione root per calcolare il Merkle Root 
+     * e restituisce true se il Merkle Root calcolato corrisponde a quello passato come argomento.
+     * 
+     * @param array $data Array di hash
+     * @param array $root merkle root
+     * @return boolean 
+     */
+    public function verify(array $data, string $root): bool
+    {
+        return ($this->root($data) === $root);
+    }
+}
